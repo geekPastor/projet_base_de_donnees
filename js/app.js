76643134f07d3efcc7e58636b3ae0faa7b725821
Chrinovic MM <20mm364@esisalama.org>
2022-08-10 12:43:28 +0200

    Ajout de quelques modificqtions sur la section  team

--- js/app.js
+++ js/app.js
@@ -10,3 +10,24 @@ function toggleMenu(){
 }
 
 toggleMenu();
+
+
+
+//les animations de la section team
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
